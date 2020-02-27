18a2a4362cc71a765397f44d2d4165116d618d97
ShayWellington <swillougg@gmail.com>
2020-02-27 15:19:28 +1300

    Added server

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
-// core modules
-const http = require('http'); // provides server
+//core modules
+ const http = require('http'); //provides server
+ const fs = require('fs'); //use this for file transaction
+ const path = require('path');
 
-const server = http.createServer((req, res) =>{
+ const server = http.createServer((req, res)=>{
+   // res.writeHead(200 , {'Content-Type' : 'text/plain'})
+   // res.write("Hello");
+   // res.end();
+   console.log(`${req.method} request for ${req.url} `);
 
-  res.write("Hello");
-  res.end();
+   if (req.method === 'GET'){
+     if (req.url === '/') {
+       fs.readFile('./public/index.html','UTF-8', (err,data)=>{
+         if (err) throw err;
+         res.writeHead(200, {'Content-Type' : 'text/html'});
+         res.end(data);
+       })
+     } else if (req.url === '/index.html') {
+          fs.readFile('./public/index.html','UTF-8', (err,data)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.end(data);
+          })
+        }  else if (req.url === '/about.html') {
+          fs.readFile('./public/about.html','UTF-8', (err,data)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.end(data);
+          })
+        } else if (req.url === '/contact.html') {
+          fs.readFile('./public/contact.html','UTF-8', (err,data)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.end(data);
+          })
+        }
+   } //method
 
-});
+ });
 
-server.listen(3000);
-console.log("running node server at port 3000");
+ server.listen(3000);
+ console.log("running node server at port 3000");
